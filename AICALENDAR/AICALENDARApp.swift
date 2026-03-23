92a986b140889d7d0713b06eee3a5a28475f5614
Utkarsh Tewari <utkarsh.tewari@hotmail.com>
2026-03-23 01:59:36 -0700

    Initial Commit

---
+++ AICALENDAR/AICALENDARApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICALENDARApp.swift
+//  AICALENDAR
+//
+//  Created by Utkarsh Tewari on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct AICALENDARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
